78df451af051cb101dd58f77d56ca73d1d7b8678
yeshwanthrajr <ryeshwanthraj29@gmail.com>
2026-04-17 01:48:10 +0530

    Fix Vercel configuration and React API Url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Maps perfectly to Node backend
+  baseURL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
+    ? 'http://localhost:5000/api' 
+    : 'https://mindsentinel-dashboard-1.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
